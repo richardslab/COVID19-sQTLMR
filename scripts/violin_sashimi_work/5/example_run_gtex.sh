ca65d1b0ffa6c1ed2ba17996cc84c07fa20910b0
juliandwillett <81776781+juliandwillett@users.noreply.github.com>
2023-05-22 18:48:17 -0400

    Final version

--- scripts/violin_sashimi_work/5/example_run_gtex.sh
+++ scripts/violin_sashimi_work/5/example_run_gtex.sh
@@ -6,13 +6,16 @@
 
 export PATH=~/miniconda3/bin:$PATH
 source activate ggsashimi
+module load r/4.1.2 r-bundle-bioconductor/3.14
 
-#bam_input=chr1_variant1_input_bams_gtex.tsv ; curr_coord="1:155162000-155162600" ; MC=30 ; palette=palette2.txt #MUC1
-#bam_input=chr1_variant2_input_bams_gtex.tsv ; curr_coord="1:156203400-156206300" ; MC=10 ; palette=palette1_2.txt #PMF1
-#bam_input=chr4_input_bams_gtex.tsv ; curr_coord="4:106818000-106850000" ; MC=10 ; palette=palette12.txt
-#bam_input=chr12_input_bams_gtex.tsv ; curr_coord="12:113355405-113357400" ; MC=1 ; palette=palette12.txt
-#bam_input=chr13_input_bams_gtex.tsv ; curr_coord="13:113530100-113535060" ; MC=10 ; palette=palette19.txt
-#bam_input=chr19_input_bams_gtex.tsv ; curr_coord="19:4714049-4718227" ; MC=3 ; palette=palette19.txt #DPP9
+bam_input=chr1_variant1_input_bams_gtex.tsv ; curr_coord="1:155162000-155162600" ; MC=30 ; palette=palette2.txt #MUC1
+bam_input=chr1_variant2_input_bams_gtex.tsv ; curr_coord="1:156203400-156206300" ; MC=10 ; palette=palette1_2.txt #PMF1
+bam_input=chr4_input_bams_gtex.tsv ; curr_coord="4:106818000-106850000" ; MC=10 ; palette=palette12.txt
+bam_input=chr12_input_bams_gtex.tsv ; curr_coord="12:113355405-113357400" ; MC=1 ; palette=palette12.txt
+###bam_input=chr13_input_bams_gtex.tsv ; curr_coord="13:113530100-113535060" ; MC=10 ; palette=palette19.txt
+bam_input=chr13_input_bams_gtex.tsv ; curr_coord="13:113530100-113536190" ; MC=10 ; palette=palette19.txt
+###bam_input=chr19_input_bams_gtex.tsv ; curr_coord="19:4714049-4718227" ; MC=3 ; palette=palette19.txt #DPP9
+bam_input=chr19_input_bams_gtex.tsv ; curr_coord="19:4714049-4720000" ; MC=3 ; palette=palette19.txt #DPP9 extended
 
 #go to line 1026 to adjust order
 
