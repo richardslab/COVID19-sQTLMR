82c2cf52c05491a809dcc7dfb8ab4cc12ba0bba4
juliandwillett <81776781+juliandwillett@users.noreply.github.com>
2023-08-17 19:21:14 -0400

    Update Isolate_Variants.bash

--- scripts/violin_sashimi_work/1/Isolate_Variants.bash
+++ scripts/violin_sashimi_work/1/Isolate_Variants.bash
@@ -1,27 +1,27 @@
-export PATH=~/projects/richards/share/bin:$PATH
+#!/bin/bash
+
+#GRCh38
 
 bcftools view \
-GTEx_Analysis_20150112_OMNI_2.5M_5M_450Indiv_chr1to22_phased_genot_imput_info04_maf01_HWEp1E6_ConstrVarIDs.vcf.gz \
---regions 1:155162067 --output-type z --output Output/chr1_variant1_var.vcf.gz
+GTEx_Analysis_2017-06-05_v8_WholeGenomeSeq_838Indiv_Analysis_Freeze.SHAPEIT2_phased.vcf.gz \
+--regions chr1:155192276 --output-type z --output Output/chr1_variant1_var.vcf.gz
 
 bcftools view \
-GTEx_Analysis_20150112_OMNI_2.5M_5M_450Indiv_chr1to22_phased_genot_imput_info04_maf01_HWEp1E6_ConstrVarIDs.vcf.gz \
---regions 1:156206121 --output-type z --output Output/chr1_variant2_var.vcf.gz
+GTEx_Analysis_2017-06-05_v8_WholeGenomeSeq_838Indiv_Analysis_Freeze.SHAPEIT2_phased.vcf.gz \
+--regions chr1:156236330 --output-type z --output Output/chr1_variant2_var.vcf.gz
 
 bcftools view \
-GTEx_Analysis_20150112_OMNI_2.5M_5M_450Indiv_chr1to22_phased_genot_imput_info04_maf01_HWEp1E6_ConstrVarIDs.vcf.gz \
---regions 4:106819053 --output-type z --output Output/chr4_var.vcf.gz
+GTEx_Analysis_2017-06-05_v8_WholeGenomeSeq_838Indiv_Analysis_Freeze.SHAPEIT2_phased.vcf.gz \
+--regions chr4:105897896 --output-type z --output Output/chr4_var.vcf.gz
 
 bcftools view \
-GTEx_Analysis_20150112_OMNI_2.5M_5M_450Indiv_chr1to22_phased_genot_imput_info04_maf01_HWEp1E6_ConstrVarIDs.vcf.gz \
---regions 12:113357193 --output-type z --output Output/chr12_var.vcf.gz
+GTEx_Analysis_2017-06-05_v8_WholeGenomeSeq_838Indiv_Analysis_Freeze.SHAPEIT2_phased.vcf.gz \
+--regions chr12:112919388 --output-type z --output Output/chr12_var.vcf.gz
 
 bcftools view \
-GTEx_Analysis_20150112_OMNI_2.5M_5M_450Indiv_chr1to22_phased_genot_imput_info04_maf01_HWEp1E6_ConstrVarIDs.vcf.gz \
---regions 13:113535741 --output-type z --output Output/chr13_var.vcf.gz
+GTEx_Analysis_2017-06-05_v8_WholeGenomeSeq_838Indiv_Analysis_Freeze.SHAPEIT2_phased.vcf.gz \
+--regions chr13:112881427 --output-type z --output Output/chr13_var.vcf.gz
 
 bcftools view \
-GTEx_Analysis_20150112_OMNI_2.5M_5M_450Indiv_chr1to22_phased_genot_imput_info04_maf01_HWEp1E6_ConstrVarIDs.vcf.gz \
---regions 19:4717672 --output-type z --output Output/chr19_var.vcf.gz
-
-#exome gatk output is grch37, not grch38
+GTEx_Analysis_2017-06-05_v8_WholeGenomeSeq_838Indiv_Analysis_Freeze.SHAPEIT2_phased.vcf.gz \
+--regions chr19:4717660 --output-type z --output Output/chr19_var.vcf.gz
